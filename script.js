7f98727b6fae9ece1b2ef355d4e1daa6b71691bb
Ifure Udoh <Ifureudoh08@gmail.com>
2021-09-22 08:37:26 +0100

    first commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ setTimeout(getData, 2500)
 
 function getData() {
   header.innerHTML =
-    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
+    '<img src="https://images.unsplash.com/photo-1632196068933-23998d66a2dd?ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" />'
   title.innerHTML = 'Lorem ipsum dolor sit amet'
   excerpt.innerHTML =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
